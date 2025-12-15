4cdb2dc30f2364e198012fde7f05ba2a54e4fa21
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-15 13:35:35 +0100

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+namespace ePaper {
+    /**
+     * Test function
+     */
+    //% blockId= ePaperTest
+    //% block="test $i"
+    //$ i.min=0 i.max=9
+    export function test(i: number) : number {
+        return i + 1
+    }
+}

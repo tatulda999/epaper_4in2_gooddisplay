abd535c2334a448f12b3afbc15077ddb45a038e3
tatulda999 <65970263+tatulda999@users.noreply.github.com>
2025-12-22 17:30:27 +0100

    Update display.cpp, shims.d.ts

--- shims.d.ts
+++ shims.d.ts
@@ -1,2 +1,14 @@
+// Auto-generated. Do not edit.
 declare namespace ePaper {
+
+    /**
+     * Initialize the ADC to be able to measure Vdd voltage.
+     *
+     * To do this it initialises and finds the ADC channel for pin P0,
+     * which is hijacked by get_vdd_millivolts() to measure Vdd instead.
+     */
+    //% shim=ePaper::clear
+    function clear(color: uint8): void;
 }
+
+// Auto-generated. Do not edit. Really.
